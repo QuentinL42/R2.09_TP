acfe12389b7a64769e2da674cfb9394feaad32e8
lq01385a <quentin.lapasin@etu.univ-st-etienne.fr>
2026-06-09 15:12:18 +0200

    ex_2_finish

--- src/Controller/TP1Ex1Q1Controller.php
+++ src/Controller/TP1Ex1Q1Controller.php
@@ -3,16 +3,44 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
-final class TP1Ex1Q1Controller extends AbstractController
+class TP1Ex1Q1Controller extends AbstractController
 {
-    #[Route('/t/p1/ex1/q1', name: 'app_t_p1_ex1_q1')]
-    public function index(): Response
+    #[Route('/', name: 'accueil')]
+    public function accueil(): Response
     {
-        return $this->render('tp1_ex1_q1/index.html.twig', [
-            'controller_name' => 'TP1Ex1Q1Controller',
+        return $this->render('tp1/accueil.html.twig');
+    }
+
+    #[Route('/date1', name: 'date1')]
+    public function date1(): Response
+    {
+        $dateDuJour = date('d/m/Y H:i:s');
+
+        return $this->render('tp1/date1.html.twig', [
+            'dateDuJour' => $dateDuJour
+        ]);
+    }
+
+    #[Route('/date2', name: 'date2')]
+    public function date2(): Response
+    {
+        return $this->render('tp1/date2.html.twig');
+    }
+
+    #[Route('/bonjour', name: 'bonjour', methods: ['POST'])]
+    public function bonjour(Request $request): Response
+    {
+        $prenom = $request->request->get('prenom');
+        $nom = $request->request->get('nom');
+
+        return $this->render('tp1/bonjour.html.twig', [
+            'prenom' => $prenom,
+            'nom' => $nom,
+            'dateDuJour' => date('d/m/Y H:i:s')
         ]);
     }
 }
